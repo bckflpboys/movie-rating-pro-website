ddce708a9c7a2598e5359acea25bb6a51f8062f9
bckflpboys <crushadde@gmail.com>
2025-12-28 19:15:10 +0200

    use cases

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import DeepDive from "./components/DeepDive";
+import UseCases from "./components/UseCases";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Features />
       <DeepDive />
+      <UseCases />
     </main>
   );
 }
